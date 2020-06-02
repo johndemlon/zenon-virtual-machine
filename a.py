f59bffdd7271f26026218631a0e13e961b57be6e
john demlon <53253324+johndemlon@users.noreply.github.com>
2020-06-02 09:17:43 +0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,12 @@
+while(1):
+    a = input("> ")
+    if(a=="start"):
+        print("started")
+    elif(a=="pan"):
+        print("paning...")
+    elif(a=="ex"):
+        print("exiting...")
+        exit()
+    else:
+        print("invalid block")
+
